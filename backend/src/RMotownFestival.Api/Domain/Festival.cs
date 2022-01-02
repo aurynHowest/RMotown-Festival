09e0bb5ed8f62df893dc82a88b16bf72dff1e405
auryn.vansteenkiste <auryn.vansteenkiste@student.howest.be>
2022-01-02 18:04:40 +0100

    initt

---
+++ backend/src/RMotownFestival.Api/Domain/Festival.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RMotownFestival.Api.Domain
+{
+    public class Festival
+    {
+        public int Id { get; set; }
+
+        public Schedule LineUp { get; set; }
+
+        public ICollection<Artist> Artists { get; set; }
+
+        public ICollection<Stage> Stages { get; set; }
+
+        public Festival()
+        {
+            Artists = new List<Artist>();
+            Stages = new List<Stage>();
+        }
+    }
+}
---
+++ frontend/RMotownFestival/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  festivalName: "RMotownFestival",
+  apiBaseUrl: "https://rmotownfestivalapiav.azurewebsites.net/api/settings"
+};
